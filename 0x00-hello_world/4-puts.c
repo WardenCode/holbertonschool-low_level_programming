5d52aeb95c2350d1b1ceebdb8e3d3a45f633fbc3
Diego <diegojeanluck@hotmail.com>
2022-02-03 18:23:17 -0500

    fix now follows the betty standar

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-int main(void) {
+/**
+ * main - Display a message and return 0
+ *
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
