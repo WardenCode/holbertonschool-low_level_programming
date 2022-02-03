83035c1894db042a4404427bb3d5eae5e4160964
Diego <diegojeanluck@hotmail.com>
2022-02-03 18:46:18 -0500

    feat mandatory task5 and works

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Display a message and return 0
+ *
+ * Description: Use printf for display a message to the terminal
+ * "with proper grammar, but the outcome is a piece of art," and a new line
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
