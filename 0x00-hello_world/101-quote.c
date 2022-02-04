f63a71cbae0429a6963e430aa1fcb532e921d1cc
Diego <diegojeanluck@hotmail.com>
2022-02-03 20:49:39 -0500

    fix advanced task101 now have the expected behavior

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
  * main - prints an error message
- *   
+ *
  * Description: Prints an error message:
  * "and that piece of art is useful\" - Dora Korpar, 2015-10-19".
- * 
+ *
  * Return: Always return 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str1[35] = "and that piece of art is useful\" ";
+	char str2[28] = "- Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s%s", str1, str2);
 	return (1);
 }
