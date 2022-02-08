c9c06d05b2f35f5e1cc3e80bc0aa9ec811ba0145
Diego <diegojeanluck@hotmail.com>
2022-02-08 17:27:27 -0500

    feat mandatory task 1 and works

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Description: Prints the alphabet in lowercase by a new line.
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+		_putchar(10);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
