d8c63f879d32886eabeb8f380094c0e2c8af368f
Diego <diegojeanluck@hotmail.com>
2022-02-08 15:41:29 -0500

    feat mandatory task0 and works

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "_putch.h"
+
+/**
+ * print_text - Prints "_putchar"
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ *
+ * Return: void
+ */
+
+void print_text(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	print_text();
+	return (0);
+}
