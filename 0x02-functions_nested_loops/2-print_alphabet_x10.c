48bdd6c7418cf5fd1334c08bda04f1f6953ee2c3
Diego <diegojeanluck@hotmail.com>
2022-02-08 18:00:21 -0500

    feat mandatory task 2 and works

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase 10 times
+ *
+ * Description: Prints the alphabet in lowercase by a new line 10 times.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i<=10 ; i++)
+	{
+		print_alphabet();
+	}
+
+}
