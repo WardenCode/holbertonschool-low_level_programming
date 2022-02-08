fedba6235a1a699bd1699bb92f1138621ad1e135
Diego <diegojeanluck@hotmail.com>
2022-02-08 18:12:04 -0500

    fix mandatory task 2 now have the expexted behavior

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,21 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase 10 times
  *
- * Description: Prints the alphabet in lowercase by a new line 10 times.
+ * Description: Prints the alphabet in lowercase by a new line 10  * times.
  *
  * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i<=10 ; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		print_alphabet();
+		for(j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(10);
 	}
-
 }
