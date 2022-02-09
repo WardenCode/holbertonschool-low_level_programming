ed36444a1e8e46e1735be42afad0b1c974b91ee8
Diego <diegojeanluck@hotmail.com>
2022-02-08 22:07:31 -0500

    feat mandatory task 7 and works

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Print the last digit of a number
+ *
+ * @n: Number recevied
+ *
+ * Return: return the last digit of a number
+ */
+
+int _abs(int n)
+{
+	int result;
+
+	result = n % 10;
+
+	return result;
+}
