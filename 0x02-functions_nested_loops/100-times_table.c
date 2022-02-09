f5c35d2f1cdb412880588529e2c0084cc38cca01
Diego <diegojeanluck@hotmail.com>
2022-02-09 16:58:45 -0500

    feat advanced task 100 and works

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_times_tables - prints the n times table, starting with 0.
+ *
+ * @n: number of times to print the table, min-max(1-14)
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				result = i * j;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				else if (result >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + result / 100);
+					_putchar('0' + ((result / 10) % 10));
+					_putchar('0' + result % 10);
+				}
+				else if (result >= 10 && result <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
+				}
+				else if (result < 10 && result >= 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + result);
+				}
+			}
+			_putchar(10);
+		}
+	}
+
+}
