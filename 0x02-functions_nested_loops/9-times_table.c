af8c811cc70df1ffad49b8d05a5b859d48670ea2
Diego <diegojeanluck@hotmail.com>
2022-02-09 00:17:06 -0500

    feat mandatory task 9 and works

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Print the last digit of a number
+ *
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; i < 10; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (result > 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			else if (result < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+		}
+		_putchar(10);
+	}
+}
