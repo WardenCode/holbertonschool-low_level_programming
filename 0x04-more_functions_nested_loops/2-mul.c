2daffa946561ddfe22fe13df84796e7bde02be92
Diego <diegojeanluck@hotmail.com>
2022-02-10 17:01:04 -0500

    feat mandatory task 2 and works

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 numbers.
+ *
+ * @a: number
+ *
+ * @b: number
+ *
+ * Return: 1 if c is a digit, otherwise return 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
