fb3fc0e441fa2dcd7866cec37006ec42effb1714
Diego <diegojeanluck@hotmail.com>
2022-02-10 22:58:54 -0500

    feat mandatory task 6 and works

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ *
+ * Return: Void
+ */
+
+void print_line(int n)
+{
+	int counter = 0;
+
+	if (n > 0)
+	{
+		while(counter < n)
+		{
+			_putchar('_');
+			counter++;
+		}
+	}
+	_putchar('\n');
+}
