f3d428f196ad0f95375cc3404c40f47eba2097db
Diego <diegojeanluck@hotmail.com>
2022-02-14 17:04:57 -0500

    fixed now follow the rules of betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it
- * points to to 98.
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * it points to to 98.
  *
- * @*n: pointer to an int variable.
+ * @n: pointer to an int variable.
  *
  * Return: Void
  */
