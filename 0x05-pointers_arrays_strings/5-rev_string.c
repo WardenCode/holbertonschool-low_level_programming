0a4cadd71e541e0812f95fa9ee4179ca4f6f2e28
Diego <diegojeanluck@hotmail.com>
2022-02-14 22:44:55 -0500

    feat mandatory task #5 and works

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string.
+ *
+ * @s: String.
+ *
+ * Return: Void
+ */
+
+void rev_string(char *s)
+{
+	int size = 0;
+	char *second_pointer = s;
+	char tmp;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	s--;
+	size--;
+
+	if (size % 2 != 0)
+	{
+		while (second_pointer + 1 != s)
+		{
+			tmp = *second_pointer;
+			*second_pointer = *s;
+			*s = tmp;
+			s--;
+			second_pointer++;
+		}
+	}
+	else
+	{
+		while (second_pointer != s)
+		{
+			tmp = *second_pointer;
+			*second_pointer = *s;
+			*s = tmp;
+			s--;
+			second_pointer++;
+		}
+	}
+}
