83c09dde03103faef62141c3a6bd10f0cc298067
Diego <diegojeanluck@hotmail.com>
2022-02-16 18:22:59 -0500

    feat mandatory task #1 and works

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ *
+ * @dest: string-base.
+ *
+ * @src: string to add in the string-base.
+ *
+ * @n: cuantity of letters to use of src.
+ *
+ * Return: Void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int size_dest, size_src, iterator_src, tmp;
+
+	for (size_dest = 0; *(dest + size_dest) != '\0'; size_dest++)
+		;
+
+	for (size_src = 0; *(src + size_src) != '\0'; size_src++)
+		;
+
+	tmp = (n > size_src) ? size_src : n;
+
+	for (iterator_src = 0; iterator_src != tmp; iterator_src++)
+	{
+		*(dest + size_dest + iterator_src) = *(src + iterator_src);
+	}
+
+	*(dest + size_dest + iterator_src + 1) = '\0';
+
+	return (dest);
+}
