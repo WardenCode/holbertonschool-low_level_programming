fa33523f11b0151a152aecf23353126c2d6aee4c
Diego <diegojeanluck@hotmail.com>
2022-02-17 10:34:48 -0500

    Feat mandatory task #2 and works

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ *
+ * @dest: string-base.
+ *
+ * @src: string to replce the string-base.
+ *
+ * @n: cuantity of letters to use of src.
+ *
+ * Return: Void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, size_dest;
+
+	for (size_dest = 0; *(dest + size_dest) != '\0'; size_dest++)
+		;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	for (; i < n && i < size_dest; i++)
+	{
+		*(dest + i) = '\0';
+	}
+
+	return (dest);
+}
