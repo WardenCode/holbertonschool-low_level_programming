d2fb2a7b83d153244a681d4e7589abd8c1b4577f
Diego <diegojeanluck@hotmail.com>
2022-02-17 18:59:20 -0500

    feat mandatory task #6 and works

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize the string.
+ *
+ * @text: Pointer to string
+ *
+ * Return: The string in uppercase
+ */
+
+char *cap_string(char *text)
+{
+	int chars[13] = {' ', '.', ',', '\n', ';', '(', ')', '!', '?',
+		'"', '{', '}', '\t'};
+	int i = 0;
+	int j;
+
+	while (text[i] != '\0')
+	{
+		j = 0;
+		if (i == 0)
+		{
+			if (text[i] >= 'a' && text[i] <= 'z')
+				text[i] -= 32;
+			i++;
+			continue;
+		}
+
+		while (j < 13)
+		{
+			if (text[i] == chars[j])
+			{
+				if (text[i + 1] >= 'a' && text[i + 1] <= 'z') {
+					text[i + 1] -= 32;
+					break;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (text);
+}
