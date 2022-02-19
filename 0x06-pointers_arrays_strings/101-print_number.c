299ba59fd7881d7dcdc0abb69b9bca456f257e9e
Diego <diegojeanluck@hotmail.com>
2022-02-18 23:16:55 -0500

    fix advanced task #101 now have the correct documentation and now takes the MIN_INT and MAX_INT.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,19 +3,22 @@
 /**
  * print_number - Prints an integer.
  *
+ * @n: int to print
+ *
  * Return: Always 0
  */
 
 void print_number(int n)
 {
+	unsigned int i = n;
+	
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (i / 10)
+		print_number(i / 10);
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (i % 10));
 }
