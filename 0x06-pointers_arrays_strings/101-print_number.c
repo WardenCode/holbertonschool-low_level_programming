6d3685ae93e64a8ccf4986734c2835b50b660ac8
Diego <diegojeanluck@hotmail.com>
2022-02-18 23:19:32 -0500

    back to when it worked and has more ckecks of the checker

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,13 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
-	
 	if (n < 0)
 	{
 		_putchar('-');
 	}
 
-	if (i / 10)
-		print_number(i / 10);
+	if (n / 10)
+		print_number(n / 10);
 
-	_putchar('0' + (i % 10));
+	_putchar('0' + (n % 10));
 }
