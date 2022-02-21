cb2e5f72e475586c8841e0c8dabcbde90affe6a5
Diego <diegojeanluck@hotmail.com>
2022-02-21 12:12:53 -0500

    Added the rest of the files for the project

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two string
+ *
+ * @s: Pointer to the space of memory.
+ *
+ * @b: Constant byte.
+ *
+ * @n: Bytes of the memory area.
+ *
+ * Return: Pointer to the memory area s.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return ();
+}
