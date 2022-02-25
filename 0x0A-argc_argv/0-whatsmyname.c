b44d527f62478ecc519cec48dfe67c8707d7ced8
Diego <diegojeanluck@hotmail.com>
2022-02-25 11:09:45 -0500

    feat taks #0 print my own name (file)

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints its own name.
+ *
+ * @argc: number of parameters
+ *
+ * @argv: parameters recevied.
+ *
+ * Return: EXIT_SUCCESS
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
