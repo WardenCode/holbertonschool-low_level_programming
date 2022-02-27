e0f59f6de91ff4cb66f939d927e5f37ad0668fb5
Diego <diegojeanluck@hotmail.com>
2022-02-26 20:36:04 -0500

    fix betty style and now handle the 0 case

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_tables - prints the n times table, starting with 0.
+ * print_times_table - prints the n times table, starting with 0.
  *
  * @n: number of times to print the table, min-max(1-14)
  *
@@ -19,38 +20,35 @@ void print_times_table(int n)
 			for (j = 0; j < n + 1; j++)
 			{
 				result = i * j;
-
 				if (j == 0)
-				{
 					_putchar('0');
-				}
-				else if (result >= 100)
+				_putchar(',');
+				_putchar(' ');
+				if (result >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar('0' + result / 100);
 					_putchar('0' + ((result / 10) % 10));
 					_putchar('0' + result % 10);
 				}
 				else if (result >= 10 && result <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + result / 10);
 					_putchar('0' + result % 10);
 				}
 				else if (result < 10 && result >= 0)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + result);
 				}
 			}
-			_putchar(10);
+			_putchar('\n');
 		}
 	}
-
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
 }
