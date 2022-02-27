91e05ed2cd3f6b054f9da5096808bfad01ed4c43
Diego <diegojeanluck@hotmail.com>
2022-02-26 20:16:39 -0500

    feat advanced task #101 and works

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,16 +3,30 @@
 /**
  * print_number - Prints an integer.
  *
- * @n: int to print
+ * @n: number to print
  *
  * Return: Always 0
  */
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int new_number = 0;
+
+	if (n == -2147483648)
+	{
+		new_number = n;
+		_putchar('-');
+		new_number = -new_number;
+
+		if (new_number / 10)
+			print_number(new_number / 10);
+		_putchar('0' + (new_number % 10));
+		return;
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
+		n = -n;
 	}
 
 	if (n / 10)
