07fb99cee6e2430c6a00b67e1d7c8a11a4a2d71e
Diego <diegojeanluck@hotmail.com>
2022-02-27 11:32:53 -0500

    feat advanced task #103 and works

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints the sum of the even-valued terms.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1, b = 2, sum = 0, result = 0;
+
+	while (sum < 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if (sum % 2 == 0)
+			result += sum;
+	}
+	printf("%d\n", result);
+	return (0);
+}
