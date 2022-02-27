f6fc98ab4132da50ecad9458ff90c8b0d3f93663
Diego <diegojeanluck@hotmail.com>
2022-02-27 15:53:39 -0500

    fix advanced task now have the expected behavior, but is not effective

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,23 +5,43 @@
  *
  * Return: 0
  */
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: end program
+ */
 int main(void)
 {
-	unsigned long int a = 1, b = 2, sum = 0, counter = 0;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%lu, %lu, ", a, b);
+	j = 1;
+	k = 2;
 
-	while (counter < 96)
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		sum = a + b;
-		a = b;
-		b = sum;
-		if (counter != 95)
-			printf("%lu, ", sum);
-		else
-			printf("%lu\n", sum);
-		counter++;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+	printf("\n");
+
 	return (0);
 }
