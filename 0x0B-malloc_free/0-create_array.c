466d17af842063945076a2d3cdd4310404a11de5
Diego <diegojeanluck@hotmail.com>
2022-03-01 12:02:49 -0500

    fix betty style and now have a for one line

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,10 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(size * sizeof(char));
 
 	if (size == 0 || array == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
-	{
 		array[i] = c;
-	}
 
 	return (array);
 }
