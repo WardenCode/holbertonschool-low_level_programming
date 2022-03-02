116eec3b75206fe16c12cde98fc849b39e16d739
Diego <diegojeanluck@hotmail.com>
2022-03-01 22:03:21 -0500

    fix betty style and refactoring code, now works good

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * argstostr - Concatenates all the arguments of your program.
+ *
+ * @ac: arg count
+ *
+ * @av: arg vector
+ *
+ * Return: Pointer to a new string
+ */
+
 char *argstostr(int ac, char **av)
 {
 	int i, j, size = 0, count = 0;
