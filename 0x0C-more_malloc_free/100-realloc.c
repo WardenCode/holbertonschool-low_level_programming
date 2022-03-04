c7176a8b0e7ccb0d514bd2c3f3ce86bf5f45a4e7
Diego <diegojeanluck@hotmail.com>
2022-03-03 22:31:09 -0500

    delete dont trust on malloc() and refactor code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * dont_trust_on_malloc - Free a pointer in case of malloc fails.
- *
- * @pointer: Pointer to test.
- *
- * Return: If all go good return 0 otherwise is 1.
- */
-
-int dont_trust_on_malloc(void *pointer)
-{
-	if (pointer == NULL)
-	{
-		free(pointer);
-		return (1);
-	}
-	return (0);
-}
-
-/**
  * _realloc - function that reallocates a memory block using malloc and free
  *
  * @ptr: Pointer to the memory previously allocated with a call to malloc.
@@ -36,18 +18,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *resize_ptr = NULL;
 	unsigned int i = 0;
 
-	if (new_size == old_size || old_size > new_size)
-		return (ptr);
-
-	if (original_ptr == NULL && old_size == 0 && new_size > 0)
+	if (original_ptr == NULL)
 	{
 		resize_ptr = malloc(new_size);
-		if (dont_trust_on_malloc(resize_ptr) == 1)
+		if (!resize_ptr)
 			return (NULL);
-		for (; i < new_size; i++)
-			resize_ptr[i] = 0;
-		free(ptr);
+		return (resize_ptr);
 	}
+
+	if (new_size == old_size || old_size > new_size)
+		return (ptr);
 
 	if (new_size == 0 && original_ptr != NULL)
 	{
@@ -59,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		resize_ptr = malloc(new_size);
 
-		if (dont_trust_on_malloc(resize_ptr) == 1)
+		if (!resize_ptr)
 			return (NULL);
 
 		for (; i < old_size; i++)
@@ -67,7 +47,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		for (; i < new_size; i++)
 			resize_ptr[i] = 0;
-
 		free(ptr);
 	}
 	return (resize_ptr);
