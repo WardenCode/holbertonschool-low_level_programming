b9c21a2b9e05881f3be26f9be18ab75e5032d0a7
Diego <diegojeanluck@hotmail.com>
2022-03-07 15:54:39 -0500

    update with the stdlib and stdio

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,9 @@ struct dog
 	char *owner;
 };
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /* dog_t *new_dog(char *name, float age, char *owner); */
