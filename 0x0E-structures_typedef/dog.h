2ba3e602e269c8faa15fcfa3539429cc70275663
Diego <diegojeanluck@hotmail.com>
2022-03-07 15:01:48 -0500

    Feat mandatory task #0 and works

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Basic atributtes of a dog
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
