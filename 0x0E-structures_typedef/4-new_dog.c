90fc2ab7ab542f92bdd2dd0bca8bba1c932b5a92
Diego <diegojeanluck@hotmail.com>
2022-03-07 20:20:22 -0500

    fix betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,21 +18,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p = &new_dog;
 	int i, size_name = 0, size_owner = 0;
 
-	for (i = 0; name[i]; i++);
+	for (i = 0; name[i]; i++)
+		;
 	size_name = i;
 
-	for (i = 0; owner[i]; i++);
+	for (i = 0; owner[i]; i++)
+		;
 	size_owner = i;
 
 	p->name = malloc(size_name * sizeof(char));
 	if (!p->name)
-		return(NULL);
+		return (NULL);
 
 	p->owner = malloc(size_owner * sizeof(char));
 	if (!p->owner)
 	{
 		free(p->name);
-		return(NULL);
+		return (NULL);
 	}
 
 	for (i = 0; i <= size_name; i++)
