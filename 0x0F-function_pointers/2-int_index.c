1e1083aa520aad1899d230a7a4473bdaf409ea15
Diego <diegojeanluck@hotmail.com>
2022-03-09 15:07:17 -0500

    fix betty style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, result_actual_element = 0;
 
-	if(size <= 0 || !array || !cmp)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 
 	for (i = 0; i < size; i++)
@@ -25,7 +25,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		result_actual_element = cmp(array[i]);
 
 		if (result_actual_element != 0)
-			return(i);
+			return (i);
 	}
 	return (-1);
 }
