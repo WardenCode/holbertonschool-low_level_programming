a6a02bc2aaea4ea08de78f5bd86ec5cfc6048ee8
Diego <diegojeanluck@hotmail.com>
2022-03-09 12:48:01 -0500

    Feat: mandatory task #0 and works

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "pointers.h"
+
+/**
+ * print_name - Prints a name
+ *
+ * @name: Name to print.
+ *
+ * @f: Pointer to a function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
