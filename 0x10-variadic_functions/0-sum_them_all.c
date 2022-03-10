30d404425fbe3c1bbaeb16eed59e149a29cd9db0
Diego <diegojeanluck@hotmail.com>
2022-03-10 12:21:41 -0500

    Feat mandatory task #0 and works

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "stdarg.h"
+
+/**
+ * sum_them_all - sum of all its parameters.
+ *
+ * @n: Cuantity of arguemts
+ *
+ * Return: The sum of all the numbers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
