5b630773f71e6ffc4447495c9bb5666edd851915
Diego <diegojeanluck@hotmail.com>
2022-03-10 12:23:03 -0500

    Feat mandatory task #1 and works

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_number - Print all the numbers passed like arguments
+ *
+ * @separator: String to separate numbers
+ *
+ * @n: Number of arguments
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list number;
+	unsigned int i;
+
+	va_start(number, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(number, int));
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
+	}
+
+	va_end(number);
+	putchar(10);
+}
