cd3c54fe98608db2833d6aa8366d01c95d89adb1
Diego <diegojeanluck@hotmail.com>
2022-03-09 22:05:15 -0500

    Now use the string library "strcmp"
    Signed-off-by: Diego <diegojeanluck@hotmail.com>

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,44 +1,6 @@
 #include "3-calc.h"
 
 /**
- * _strcmp - Compare a string with another
- *
- * @s1: string.
- *
- * @s2: string.
- *
- * Return: Void
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i, size1, size2;
-	int result = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	size1 = i;
-
-	for (i = 0; s2[i] != '\0'; i++)
-		;
-	size2 = i;
-
-	if (size1 != size2)
-		return (1);
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			result = s1[i] - s2[i];
-			break;
-		}
-	}
-
-	return (result);
-}
-
-/**
  * get_op_func - Validate and get the operation to use.
  *
  * @s: The operator (+, -, *, /, %)
@@ -60,7 +22,7 @@ int (*get_op_func(char *s))(int a, int b)
 
 	while (ops[i].op)
 	{
-		if (_strcmp(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s))
 			return (ops[i].f);
 		i++;
 	}
