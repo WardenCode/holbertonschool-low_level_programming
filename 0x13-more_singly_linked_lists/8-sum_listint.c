cc7d2cbcb36fe130caaa02dfdc9411a081cb8acc
WardenCode <diegojeanluck@hotmail.com>
2022-03-22 17:29:53 -0500

    Feat mandatory task #8 and works

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,25 @@
 #include "lists.h"
 
 /**
- * function name - desc
+ * sum_listint - The sum of the all data of the list.
  *
+ * @head: Pointer to the head of the list.
  *
- *
- * Return: 
+ * Return: Sum of the all data of the linked list.
  */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
