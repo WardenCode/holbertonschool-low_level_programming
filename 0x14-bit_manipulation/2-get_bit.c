f77fdf8043c286e1f66186a1623576f165c8bfbc
WardenCode <diegojeanluck@hotmail.com>
2022-03-24 15:02:24 -0500

    Feat mandatory task #2 and works

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,25 @@
-#include ".h"
+#include "main.h"
 
 /**
- *  - 
+ * get_bit - Gets the value of a bit at a given index.
  *
+ * @n: Number to get the bit.
  *
+ * @index: Index to search the byte.
  *
- * Return:
+ * Return: The value of a bit at a given index.
  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result = n;
+	int holder = index;
+
+	if (holder < 0)
+		return (-1);
+
+	if (index != 0)
+		result = n >> index;
+
+	return (result & 1);
+}
