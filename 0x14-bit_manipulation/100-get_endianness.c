6ab75967e5d84ebc909e68737bf337c18d1791e6
WardenCode <diegojeanluck@hotmail.com>
2022-03-24 17:33:45 -0500

    Feat advanced task #100 and works

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,15 @@
-#include ".h"
+#include "main.h"
 
 /**
- *  - 
+ * get_endianness - Checks the endianness.
  *
- *
- *
- * Return:
+ * Return: 0 if big endian, 1 if little endian.
  */
+
+int get_endianness(void)
+{
+   int16_t i = 1;
+   int8_t *p = (int8_t *) &i;
+
+   return p[0] == 1 ? 1 : 0;
+}
