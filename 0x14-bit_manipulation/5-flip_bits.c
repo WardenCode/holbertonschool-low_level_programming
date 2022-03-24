6b99a35935677bbb1e1aa071df00feb3a544a3a5
WardenCode <diegojeanluck@hotmail.com>
2022-03-24 17:19:59 -0500

    Feat mandatory task #5 and works

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,33 @@
-#include ".h"
+#include "main.h"
 
 /**
- *  - 
+ * flip_bits - Finds the number of bits you would need to flip
+ * two numbers to be equals.
  *
+ * @n: Number to compare it's bits
  *
+ * @m: Number to compare it's bits
  *
- * Return:
+ * Return: The number of bits you would need to flip to get
+ * from one number to another.
  */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int quantity_to_flip = 0;
+	unsigned int new_number = 0;
+
+	if (n == m)
+		return (0);
+
+	new_number = n ^ m;
+
+	while (new_number)
+	{
+		if (new_number & 1)
+			quantity_to_flip++;
+		new_number = new_number >> 1;
+	}
+
+	return (quantity_to_flip);
+}
