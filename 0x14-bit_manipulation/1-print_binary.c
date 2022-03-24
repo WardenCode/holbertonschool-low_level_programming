c46d56656ce235d11c1e3e68bc8670b17630659c
WardenCode <diegojeanluck@hotmail.com>
2022-03-24 14:50:00 -0500

    Feat mandatory task #1 and works

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,21 @@
-#include ".h"
+#include "main.h"
 
 /**
- *  - 
+ * print_binary - Prints the binary representation of a number.
  *
+ * @n: Number to representate on binary
  *
- *
- * Return:
+ * Return: Void
  */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
