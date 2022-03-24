fd4176d77fc8adc28b26b00b1e58f081ad65496a
WardenCode <diegojeanluck@hotmail.com>
2022-03-24 17:35:44 -0500

    Fix betty style

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,8 +8,8 @@
 
 int get_endianness(void)
 {
-   int16_t i = 1;
-   int8_t *p = (int8_t *) &i;
+	int16_t i = 1;
+	int8_t *p = (int8_t *) &i;
 
-   return p[0] == 1 ? 1 : 0;
+	return (p[0] == 1 ? 1 : 0);
 }
