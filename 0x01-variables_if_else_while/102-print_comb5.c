8a84dde1761b9f5a56d12fa070746e7c3624f387
WardenCode <diegojeanluck@hotmail.com>
2022-03-25 14:07:01 -0500

    Fix now use a different method to resolve and works

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,33 +10,23 @@
 
 int main(void)
 {
-	int s1n1 = 48;
-	int s1n2 = 48;
-	int s2n1 = 48;
-	int s2n2 = 49;
+	int first_pair = 0, second_pair = 0;
 
-	for (s1n1 = 48; s1n1 < 58; s1n1++)
-	{
-		for (s1n2 = 48; s1n2 < 58; s1n2++)
-		{
-			for (s2n1 = 48; s2n1 < 58; s2n1++)
+	for (; first_pair <= 99; first_pair++)
+		for (second_pair = 0; second_pair <= 99; second_pair++)
+			if (first_pair < second_pair)
 			{
-				for (s2n2 = 49; s2n2 < 58; s2n2++)
+				putchar((first_pair / 10) + 48);
+				putchar((first_pair % 10) + 48);
+				putchar(' ');
+				putchar((second_pair / 10) + 48);
+				putchar((second_pair % 10) + 48);
+				if (first_pair != 98 || second_pair != 99)
 				{
-					putchar(s1n1);
-					putchar(s1n2);
-					putchar(32);
-					putchar(s2n1);
-					putchar(s2n2);
-					if (!(s1n1 == 57 && s1n2 == 56 && s2n1 == 57 && s2n2 == 57))
-					{
-						putchar(44);
-						putchar(32);
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
-		}
-	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
