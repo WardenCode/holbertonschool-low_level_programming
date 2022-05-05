c3ad6b825b02bd4cb21f9218ee10eda2feacaf6d
WardenCode <diegojeanluck@hotmail.com>
2022-05-05 14:59:20 -0500

    Adding the documentation of the function

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - Free a double linked list
+ *
+ * @head: Pointer to the head of the new linked list.
+ *
+ * Return: Void.
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	if (head->next)
