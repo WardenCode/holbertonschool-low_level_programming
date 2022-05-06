388b4dae9ce75aa7c087f258eba13c085310dfaf
WardenCode <diegojeanluck@hotmail.com>
2022-05-05 20:58:45 -0500

    Using other method, validate special cases before malloc

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,43 +14,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0, flag = 0;
 	dlistint_t *new_node = NULL, *tmp = NULL;
 
-	if (!h)
-		return (new_node);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	if (!*h || idx == 0)
-	{
-		new_node = add_dnodeint(h, n);
-	}
-	else
-	{
-		new_node = malloc(sizeof(dlistint_t));
-		new_node->n = n;
-		flag = 1;
+	tmp = *h;
+	while (tmp && idx != 1)
+		tmp = tmp->next, idx--;
 
-		for (i = 0, tmp = *h; tmp && i <= idx + 1; tmp = tmp->next, i++)
-			if (tmp && i == idx)
-			{
-				tmp->prev->next = new_node;
-				tmp->prev = new_node;
-				new_node->next = tmp;
-				new_node->prev = tmp->prev;
-				flag = 0;
-				break;
-			}
-			else if (!tmp->next && i + 1 == idx)
-			{
-				free(new_node);
-				new_node = add_dnodeint_end(h, n);
-				flag = 0;
-				break;
-			}
-	}
+	if (!tmp && idx != 1)
+		return (NULL);
+	if (!tmp->next)
+		return (add_dnodeint_end(h, n));
 
-	if (flag)
-		free(new_node);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+
+	tmp->next->prev = new_node;
+	tmp->next = new_node;
 
 	return (new_node);
 }
