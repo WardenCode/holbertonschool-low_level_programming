fa2a8585ae19f32cf3a800437a82bb6e3332de8e
WardenCode <diegojeanluck@hotmail.com>
2022-05-05 22:07:18 -0500

    Feat mandatory task #8 and works

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,37 @@
 #include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = NULL;
+
+	if (!head || !(*head))
+		return (-1);
+
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp && index != 1)
+		tmp = tmp->next, index--;
+
+	if (!tmp && index != 1)
+		return (-1);
+
+	if (!tmp->next)
+	{
+		tmp->prev->next = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	tmp->next->prev = tmp->prev;
+	tmp->prev->next = tmp->next;
+	free(tmp);
+
+	return (1);
+}
