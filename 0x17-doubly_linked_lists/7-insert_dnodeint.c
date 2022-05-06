87150209ac1b7671ecf227e0fab3358dddd3ec05
WardenCode <diegojeanluck@hotmail.com>
2022-05-05 19:52:34 -0500

    Fix memory leak when we use the last index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			}
 			else if (!tmp->next && i + 1 == idx)
 			{
+				free(new_node);
 				new_node = add_dnodeint_end(h, n);
 				flag = 0;
 				break;
