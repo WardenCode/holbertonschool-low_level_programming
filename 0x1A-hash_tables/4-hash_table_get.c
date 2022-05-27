a820fb78235d2e9c8064eb32ed041c2908358e60
WardenCode <diegojeanluck@hotmail.com>
2022-05-26 21:49:51 -0500

    Feat mandatory task #4 and works

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,31 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_get - Get the value from a hash table
+ *
+ * @ht: Hash table to search into. (hash_table_t *)
+ *
+ * @key: Key you are looking for. (char *)
+ *
+ * Return: The value founded by key, NULL otherwise.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value = NULL;
+	unsigned long int index = 0;
+	hash_node_t *node = NULL;
+
+	if (!ht || !ht->array || !key)
+		return (value);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	if (!node)
+		return (value);
+
+	value = node->value;
+
+	return (value);
+}
