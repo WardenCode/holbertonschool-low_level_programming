e884b412f5f3f9124f3a3aa81408a19ed1f49052
WardenCode <diegojeanluck@hotmail.com>
2022-05-27 09:39:20 -0500

    Fix now searching in the linked list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,20 +12,25 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	char *value = NULL;
 	unsigned long int index = 0;
 	hash_node_t *node = NULL;
+	hash_node_t *tmp = NULL;
 
 	if (!ht || !ht->array || !key)
-		return (value);
+		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 
 	node = ht->array[index];
 	if (!node)
-		return (value);
+		return (NULL);
 
-	value = node->value;
+	if (strcmp(node->key, key) == 0)
+		return (node->value);
 
-	return (value);
+	for (tmp = node; tmp; tmp = tmp->next)
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+
+	return (NULL);
 }
